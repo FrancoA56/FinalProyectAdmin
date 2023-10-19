c1362fd161940adbb439175da288871d2e738cba
Franco Adamoli <franco.adamoli@gmail.com>
2023-10-19 20:17:03 -0300

    testing process.env

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,4 +1,8 @@
 import jsonServerProvider from "ra-data-json-server";
+// Carga variables de entorno desde el archivo .env
+import dotenv from 'dotenv';
+dotenv.config();
+
 const apiUrl = process.env.SERVER_URL;
 
 export const dataProvider = jsonServerProvider(
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
 
-// https://vitejs.dev/config/
+// Carga variables de entorno desde el archivo .env
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig({
     plugins: [react()],
     define: {
-        'process.env': process.env,
+        'process.env': {
+            SERVER_URL: process.env.SERVER_URL,
+            // Agrega aquí más variables de entorno si es necesario
+        },
+    },
+    build: {
+        sourcemap: true,
     },
     server: {
         host: true,
     },
     base: './',
+    esbuild: {
+        jsxInject: `import React from 'react'`,
+    },
+    // Resto de la configuración de Vite
 });
